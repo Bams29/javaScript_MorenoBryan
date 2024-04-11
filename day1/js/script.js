90b879aec5a740aa3d4bb9bfd9577a44b93a5850
Bams29 <sarmientobryan2911@gmail.com>
2024-04-11 12:52:50 -0500

    feat: :sparkles: CRUD
    estoy tratando de hacer un crud local basado en algunos tutoriales

--- day1/js/script.js
+++ day1/js/script.js
@@ -11,3 +11,49 @@ c="soy "
 d="joto"
 
 console.log(c+d);
+
+//****************FUNCION SIN RETORNO Y CON PARAMETROS ****************/
+
+function suma(a,b){
+    console.log(a+b);
+    //return(a+b);
+}
+
+suma(5,7);
+
+console.log(typeof suma(5,8)+suma(5,12))
+
+//****************FUNCION CON RETORNO Y CON PARAMETROS ****************/
+
+function suma(a,b){
+    console.log(a+b);
+    return(a+b);
+}
+
+
+//****************FUNCION CON RETORNO Y SIN PARAMETROS ****************/
+
+
+function suma(){
+    console.log(a+b);
+    return(a+b);
+}
+
+//****************FUNCION SIN RETORNO Y SIN PARAMETROS ****************/
+
+function suma(){
+    console.log(a+b);
+    
+}
+
+//*************************BUCLE FOR*********************/
+
+arreglo=[123,"pedro",true]
+
+console.log(arreglo.length);
+
+tamano= arreglo.length;
+
+for(let i=0;i<tamano;i++){
+    console.log(arreglo.length)
+}
---
+++ day1/js/tarea1.js
@@ -0,0 +1,8 @@
+// Take a value in Celsius
+let celsius = 30;
+
+// Convert Celsius to Fahrenheit
+let fahrenheit = (celsius * 9/5) + 32;
+
+// Log the result
+console.log(`${celsius}°C is equal to ${fahrenheit}°F`);
