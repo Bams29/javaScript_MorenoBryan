27d727f03d0c22736b067eb58fc8a5ed34b03c42
Bams29 <sarmientobryan2911@gmail.com>
2024-04-11 18:07:48 -0500

    build: :beers: finish proyect
    termine de crear el crud con java

--- day1/js/crud.js
+++ day1/js/crud.js
@@ -39,5 +39,111 @@ function validarFormulario(e){
 
 function agregarEmpleado() {
     ListaEmpleados.push({...objEmpleado})
+
+    mostrarEmpleados();
+
+    formulario.reset();
+
+    limpiarObjeto();
+}
+
+function limpiarObjeto() {
+    objEmpleado.id = '';
+    objEmpleado.nombre = '';
+    objEmpleado.puesto = '';
+}
+
+function mostrarEmpleados() {
+
+    limpiarHTML();
+
+    const divEmpleados = document.querySelector('.div-empleados')
+
+    ListaEmpleados.forEach( empleado =>{
+        const{id, nombre, puesto} = empleado;
+
+        const parrafo = document.createElement('p');
+        parrafo.textContent = `${id} - ${nombre} - ${puesto} - `;
+        parrafo.dataset.id = id;
+
+        const editarBoton = document.createElement('button');
+        editarBoton.onclick = () => cargarEmpleado(empleado);
+        editarBoton.textContent = 'Editar';
+        editarBoton.classList.add('btn', 'btn-editar');
+        parrafo.append(editarBoton);
+
+
+        const eliminarBoton = document.createElement('button');
+        eliminarBoton.onclick = () => eliminarEmpleado(id);
+        eliminarBoton.textContent = 'Eliminar';
+        eliminarBoton.classList.add('btn', 'btn-eliminar');
+        parrafo.append(eliminarBoton);
+
+
+        const hr = document.createElement('hr')
+
+        divEmpleados.appendChild(parrafo);
+        divEmpleados.appendChild(hr);
+
+
+
+
+
+    });
+}
+
+function cargarEmpleado(empleado) {
+
+    const {id, nombre, puesto} = empleado;
+
+    nombreInput.value = nombre;
+    puestoInput.value = puesto;
+
+    objEmpleado.id = id;
+
+    formulario.querySelector('button[type="submit"]').textContent = 'Actualizar';
+
+    editando = true;
+
+
+
+}
+
+function editarEmpleado() {
+
+    objEmpleado.nombre = nombreInput.value;
+    objEmpleado.puesto = puestoInput.value;
+
+    ListaEmpleados.map( empleado => {
+
+        if(empleado.id === objEmpleado.id) {
+            empleado.id = objEmpleado.id;
+            empleado.nombre = objEmpleado.nombre;
+            empleado.puesto = objEmpleado.puesto;
+        }
+
+    });
+
+    limpiarHTML();
+    mostrarEmpleados();
+
+    formulario.reset();
+
+
+    editando = false;
+
+}
+
+function eliminarEmpleado(id){
+    ListaEmpleados = ListaEmpleados.filter( empleado => empleado.id !== id);
+    limpiarHTML();
+    mostrarEmpleados();
+}
+
+function limpiarHTML() {
+    const divEmpleados = document.querySelector('.div-empleados');
+    while(divEmpleados.firstChild) {
+        divEmpleados.removeChild(divEmpleados.firstChild);
+    }
 }
 
